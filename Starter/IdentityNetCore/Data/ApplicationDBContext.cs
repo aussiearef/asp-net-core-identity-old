1d19ca33682403727b970d49c787ebf02ee6a986
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-02-05 19:36:53 +0300

    Test

---
+++ Starter/IdentityNetCore/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Data
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext()
+        { }
+
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
