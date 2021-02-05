33f6c69e5c228f1a1e74deab15603096fa5dc860
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-02-05 21:03:35 +0300

    SettingUpTheUserStore

--- Starter/IdentityNetCore/Data/ApplicationDBContext.cs
+++ Starter/IdentityNetCore/Data/ApplicationDBContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace IdentityNetCore.Data
 {
