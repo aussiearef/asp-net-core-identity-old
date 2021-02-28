e28d195068d84a11421ad87a32fca855bfab8a98
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-02-28 16:55:09 +0300

    Update SmtpEmailSender.cs
    Düzeltmeler

--- Starter/IdentityNetCore/Service/SmtpEmailSender.cs
+++ Starter/IdentityNetCore/Service/SmtpEmailSender.cs
@@ -10,24 +10,23 @@ namespace IdentityNetCore.Service
 {
     public class SmtpEmailSender: IEmailSender
     {
-        private readonly IOptions<SmtpOptions> options;
+        private readonly IOptions<SmtpOptions> _options;
 
         public SmtpEmailSender(IOptions<SmtpOptions> options)
         {
-            this.options = options;
+            _options = options;
         }
 
         public async Task SendEmailAsync(string fromAddress, string toAddress, string subject, string message)
         {
             var mailMessage = new MailMessage(fromAddress, toAddress, subject, message);
 
-            using (var client = new SmtpClient(options.Value.Host, options.Value.Port)
+            using var client = new SmtpClient(_options.Value.Host, _options.Value.Port)
             {
-                Credentials = new NetworkCredential(options.Value.Username, options.Value.Password)
-            })
-            {
-                await client.SendMailAsync(mailMessage);
-            }
+                Credentials = new NetworkCredential(_options.Value.Username, _options.Value.Password)
+            };
+
+            await client.SendMailAsync(mailMessage);
         }
     }
 }
