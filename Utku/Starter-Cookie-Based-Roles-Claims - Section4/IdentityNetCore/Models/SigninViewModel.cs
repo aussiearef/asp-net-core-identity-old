0f15f35370ec6c0a4e52362f08ba8376dd232636
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-07 20:36:09 +0300

    Section4

---
+++ Utku/Starter-Cookie-Based-Roles-Claims - Section4/IdentityNetCore/Models/SigninViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Models
+{
+    public class SigninViewModel
+    {
+        [Required(ErrorMessage = "User name must be provided.")]
+        [DataType((DataType.EmailAddress))]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password must be provided.")]
+        [DataType((DataType.Password))]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Utku/Starter-Cookie-Based-Roles-Claims - Section4/IdentityNetCore/Service/SmtpOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Service
+{
+    public class SmtpOptions
+    {
+        public string Host { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public int Port { get; set; }
+    }
+}
