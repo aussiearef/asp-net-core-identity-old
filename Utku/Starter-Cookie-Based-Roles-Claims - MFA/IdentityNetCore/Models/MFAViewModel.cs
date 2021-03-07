2155edeb4d3ea9e3fc77d2442f25e1ee1770285a
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-07 16:18:58 +0300

    MFA

---
+++ Utku/Starter-Cookie-Based-Roles-Claims - MFA/IdentityNetCore/Models/MFAViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Models
+{
+    public class MFAViewModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public string QRCodeUrl { get; set; }
+    }
+}
