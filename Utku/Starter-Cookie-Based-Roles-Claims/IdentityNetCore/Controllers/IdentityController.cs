acb954f1dddd8d5170376553946d47930dd1ccb7
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-07 08:47:14 +0300

    16.Implementing Sign-out

--- Utku/Starter-Cookie-Based-Roles-Claims/IdentityNetCore/Controllers/IdentityController.cs
+++ Utku/Starter-Cookie-Based-Roles-Claims/IdentityNetCore/Controllers/IdentityController.cs
@@ -122,5 +122,11 @@ namespace IdentityNetCore.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Signout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Signin");
+        }
     }
 }
