b4e5eb40c69b3ecc7514e284f4da2148ece1c621
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-10 14:25:19 +0300

    Section4

---
+++ Utku/Starter-Cookie-Based-Roles-Claims - MFA - Section4/IdentityNetCore/Service/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Service
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string fromAddress, string toAddress, string subject, string message);
+    }
+}
